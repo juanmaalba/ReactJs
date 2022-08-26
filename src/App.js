9826f913741995c355855a75d884f3a880c7db6c
JuanAlba <jalbaworkspace@gmail.com>
2022-08-26 17:05:38 -0300

    2da entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+let nombre = prompt('Ingrese su nombre')
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Holis</p>
+     <p>Bienvenido a mi nueva app {nombre}!!</p>
+     <p>Todavia no tengo mucha funcionalidad....</p>
     </div>
   );
 }
